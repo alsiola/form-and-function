6b417206804d9fe89fed51faf9f11822d8f622cb
Alex Young <alex.young@hive.hr>
2018-01-27 23:00:14 +0000

    Account for non-string values

--- src/lib/validation/required.ts
+++ src/lib/validation/required.ts
@@ -6,6 +6,7 @@ import {
     InvalidFieldResult
 } from "./typesAndGuards";
 import { Formatter, useFormatter } from "./formatter";
+import { FieldValue } from "../index";
 
 export interface NotUndefinedMessage<T = string, U = string> {
     undef?: Message<T, void, U>;
@@ -18,12 +19,12 @@ export interface NotUndefinedMessage<T = string, U = string> {
 export const required = <T>(msg?: NotUndefinedMessage<T>) => (
     { valid, invalid }: Reporters,
     formatter: Formatter<T, MessageParams<{}>>
-) => (value: string) => {
+) => (value: FieldValue | undefined) => {
     const format = useFormatter(msg, { value }, formatter);
 
     console.log({ valueRequired: value });
 
-    return typeof value !== "undefined" && value.length > 0
+    return typeof value !== "undefined" && value.toString().length > 0
         ? valid()
         : invalid(format("undef", `Required`));
 };
